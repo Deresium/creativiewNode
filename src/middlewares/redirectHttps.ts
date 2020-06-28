6e33d3083e133c2ac79bf93224b6373ed982259f
Deresium <dimitri.steinbusch@hotmail.com>
2020-06-28 18:32:52 +0200

    no-www to www

--- src/middlewares/redirectHttps.ts
+++ src/middlewares/redirectHttps.ts
@@ -3,6 +3,8 @@ import {RequestHandler} from "express";
 const redirectHttps: RequestHandler = (req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https')
         res.redirect(`https://${req.hostname}${req.url}`);
+    else if(!req.hostname.startsWith('www.'))
+        res.redirect(`https://www.${req.hostname}${req.url}`);
     else
         next();
 }
